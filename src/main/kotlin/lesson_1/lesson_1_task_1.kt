a5cb57b1951f03b804441191b0be35d717a04f1e
COBaOrNotCOBa <yureal1986@mail.ru>
2023-03-28 12:46:57 +0300

    KS-1-1 28.03.2023 add type variable

--- src/main/kotlin/lesson_1/lesson_1_task_1.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_1.kt
@@ -2,7 +2,7 @@ package lesson_1
 
 fun main(){
 
-    val numberOfOrders = 75
-    val notificationText = "Благодорим за покупку!"
+    val numberOfOrders: Int = 75
+    val notificationText: String = "Благодорим за покупку!"
 
 }
