1b45c301e54ad8ff379692d6daf20a9f45f071cc
COBaOrNotCOBa <yureal1986@mail.ru>
2023-03-29 15:14:01 +0300

    KS-2-2 29.03.2023

--- src/main/kotlin/lesson_2/KS-2-3.kt
+++ src/main/kotlin/lesson_2/KS-2-3.kt
@@ -1,2 +1,32 @@
 package lesson_2
 
+import java.time.LocalTime
+
+fun main() {
+
+    val departureTime = LocalTime.parse("09:39")
+    val minutes : Int = departureTime.hour * 60 + departureTime.minute
+    val travelTime : Int = 457
+    val arrivalTimeHour : Int
+    val arrivalTimeMinute : Int
+    val arrivalTimeHourString : String
+    val arrivalTimeMinuteString : String
+
+    arrivalTimeHour = (minutes+travelTime)/60
+    arrivalTimeMinute = (minutes+travelTime)%60
+
+    if (arrivalTimeHour<10){
+        arrivalTimeHourString = "0${arrivalTimeHour.toString()}"
+    } else{
+        arrivalTimeHourString = arrivalTimeHour.toString()
+    }
+
+    if (arrivalTimeMinute<10){
+        arrivalTimeMinuteString="0${arrivalTimeMinute.toString()}"
+    } else{
+        arrivalTimeMinuteString=arrivalTimeMinute.toString()
+    }
+
+    println("Вермя прибытия $arrivalTimeHourString:$arrivalTimeMinuteString")
+
+}
