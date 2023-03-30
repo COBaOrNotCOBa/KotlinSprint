fafb62c0e7fca0426b608ede9b44b8438cf9365a
COBaOrNotCOBa <yureal1986@mail.ru>
2023-03-30 16:02:10 +0300

    KS-3-2 30.03.2023 Surname change

--- src/main/kotlin/lesson_3/KS-3-2.kt
+++ src/main/kotlin/lesson_3/KS-3-2.kt
@@ -1,2 +1,16 @@
 package lesson_3
 
+fun main() {
+
+    var surname = "Андреева"
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    var age: Byte = 20
+
+    println("$surname $name $patronymic, возраст $age")
+
+    surname = "Сидорова"
+    age = 22
+    println("$surname $name $patronymic, возраст $age")
+
+}
