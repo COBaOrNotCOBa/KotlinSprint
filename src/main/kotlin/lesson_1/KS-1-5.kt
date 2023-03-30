803b24463c80b5cddf039956c2f88030b528991a
COBaOrNotCOBa <yureal1986@mail.ru>
2023-03-30 10:37:38 +0300

    KS-1-5 30.03.2023 Ctrl+Alt+L using :) + String.format add

--- src/main/kotlin/lesson_1/KS-1-5.kt
+++ src/main/kotlin/lesson_1/KS-1-5.kt
@@ -2,18 +2,14 @@ package lesson_1
 
 fun main() {
 
-    val seconds : Short = 6480
+    val seconds: Short = 6480
 
-    var min : Int
-    var secondsLeft : Int
+    val min: Int
+    val secondsLeft: Int
 
-    secondsLeft = seconds%60
-    min =seconds/60
+    secondsLeft = seconds % 60
+    min = seconds / 60
 
-    if (secondsLeft < 10) {
-        println("$min:0$secondsLeft")
-    } else {
-        println("$min:$secondsLeft")
-    }
+    println(String.format("%02d:%02d", min, secondsLeft))
 
 }
