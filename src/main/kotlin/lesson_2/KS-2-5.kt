41b9f0f65e854b6fa3227f045ba187260931f394
COBaOrNotCOBa <yureal1986@mail.ru>
2023-03-30 13:29:10 +0300

    KS-2-5 30.03.2023 need help =)

--- src/main/kotlin/lesson_2/KS-2-5.kt
+++ src/main/kotlin/lesson_2/KS-2-5.kt
@@ -1,2 +1,19 @@
 package lesson_2
 
+import kotlin.math.pow
+
+fun main() {
+
+    val deposit: Int = 70000
+    val interestRate: Float = 16.7f
+    val year: Byte = 20
+    //val capitalization: Short = 365
+    //val dayInYear: Short = 365
+
+    val depositResult: Double
+    depositResult = deposit * (1 + interestRate / 100.0).pow(year.toDouble())
+    //depositResult = deposit.toDouble() * (1.0 + interestRate.toDouble() / 100.0).pow(year.toDouble())
+    //depositResult = deposit.toDouble()*(1.0+interestRate.toDouble()/100.0*capitalization.toDouble()/dayInYear.toDouble()).pow(year.toDouble())
+
+    println(String.format("%.3f", depositResult))
+}
