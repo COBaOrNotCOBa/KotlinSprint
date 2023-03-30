55d20644b51ac03a96a49046a690b0a615cb194e
COBaOrNotCOBa <yureal1986@mail.ru>
2023-03-30 11:20:17 +0300

    KS-2-2 30.03.2023 Ctrl+Alt+L using :)

--- src/main/kotlin/lesson_2/KS-2-2.kt
+++ src/main/kotlin/lesson_2/KS-2-2.kt
@@ -2,18 +2,18 @@ package lesson_2
 
 fun main() {
 
-    val permanentStaff : Short = 50
-    val intern : Short = 30
-    val permanentStaffSalary : Int = 30000
-    val internSalary : Int = 20000
+    val permanentStaff: Short = 50
+    val intern: Short = 30
+    val permanentStaffSalary: Int = 30000
+    val internSalary: Int = 20000
 
-    val salaryExpensesPermanentStaff : Int
-    val salaryExpensesAllStaff : Int
+    val salaryExpensesPermanentStaff: Int
+    val salaryExpensesAllStaff: Int
     val averageSalaryAllStaff: Int
 
     salaryExpensesPermanentStaff = permanentStaffSalary * permanentStaff
     salaryExpensesAllStaff = salaryExpensesPermanentStaff + internSalary * intern
-    averageSalaryAllStaff = salaryExpensesAllStaff / (permanentStaff+intern)
+    averageSalaryAllStaff = salaryExpensesAllStaff / (permanentStaff + intern)
 
     println(salaryExpensesPermanentStaff)
     println(salaryExpensesAllStaff)
