58690fc73958a197d81a9ea20a5df190c52f7610
COBaOrNotCOBa <yureal1986@mail.ru>
2023-03-30 16:35:25 +0300

    KS-3-4 30.03.2023 Chess

--- src/main/kotlin/lesson_3/KS-3-4.kt
+++ src/main/kotlin/lesson_3/KS-3-4.kt
@@ -1,2 +1,18 @@
 package lesson_3
 
+fun main() {
+
+    var starPosition = "E2"
+    var endPosition = "E4"
+    var strokeNumber: Short = 1
+
+    println("[$starPosition-$endPosition;$strokeNumber]")
+
+    starPosition = "D2"
+    //тут нужно просто вписать D3 или алгоритм чтобы из D2 сделать D3 c шагом вперёд? )
+    endPosition = "D3"
+    strokeNumber++
+
+    println("[$starPosition-$endPosition;$strokeNumber]")
+
+}
