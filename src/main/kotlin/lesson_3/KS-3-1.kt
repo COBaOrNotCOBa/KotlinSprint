dc7db78fc04180273b1b7cb9d987f9c505c83ce4
COBaOrNotCOBa <yureal1986@mail.ru>
2023-03-30 15:13:07 +0300

    KS-3-1 30.03.2023 Greeting!

--- src/main/kotlin/lesson_3/KS-3-1.kt
+++ src/main/kotlin/lesson_3/KS-3-1.kt
@@ -1,2 +1,15 @@
 package lesson_3
 
+fun main() {
+
+    val name = "Star Lord Karl"
+    var greeting: String
+
+    greeting = "day"
+    println("Good $greeting! Dear $name!")
+
+    greeting = "evening"
+    println("Good $greeting! Dear $name!")
+
+
+}
