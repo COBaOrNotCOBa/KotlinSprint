309039edb1d02b5a84b7c9b205fc8c780e028d27
COBaOrNotCOBa <yureal1986@mail.ru>
2023-03-31 12:24:47 +0300

    KS-3-5 31.03.2023 Chess, turn split

--- src/main/kotlin/lesson_3/KS-3-5.kt
+++ src/main/kotlin/lesson_3/KS-3-5.kt
@@ -1,2 +1,12 @@
 package lesson_3
 
+fun main() {
+
+    val turn = "D2-D4;0"
+    //мне кажется здесь каким-то другим способом нужно было решать )
+    val turnReplace = turn.replace(";","-")
+    val parts = turnReplace.split("-")
+
+    for(part in parts) println(part)
+
+}
