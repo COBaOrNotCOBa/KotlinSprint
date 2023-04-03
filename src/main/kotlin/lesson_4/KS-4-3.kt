08dd26be917c0384ee8da4ae7bdb72e513b3cfe2
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-03 17:04:26 +0300

    KS-4-3 03.04.2023 Legumes

--- src/main/kotlin/lesson_4/KS-4-3.kt
+++ src/main/kotlin/lesson_4/KS-4-3.kt
@@ -1,2 +1,19 @@
 package lesson_4
 
+fun main() {
+
+    val weather = "солнечная"
+    val tent = "раскрыт"
+    val humidity = "20"
+    val season = "время года зима"
+    println(
+        "Благоприятные ли условия сейчас для роста бобовых? " +
+                "${(weather == LEGUMES_WEATHER) && (tent == LEGUMES_TENT) && (humidity == LEGUMES_HUMIDITY) && (season != LEGUMES_SEASON)}"
+    )
+
+}
+
+const val LEGUMES_WEATHER = "солнечная"
+const val LEGUMES_TENT = "раскрыт"
+const val LEGUMES_HUMIDITY = "20"
+const val LEGUMES_SEASON = "время года зима"
