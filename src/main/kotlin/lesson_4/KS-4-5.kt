560d96440018f6124b122cc3ee80ed4a0cd520b9
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-04 11:26:06 +0300

    KS-4-5 04.04.2023 See odyseey

--- src/main/kotlin/lesson_4/KS-4-5.kt
+++ src/main/kotlin/lesson_4/KS-4-5.kt
@@ -1,2 +1,20 @@
 package lesson_4
 
+fun main() {
+
+    print("Есть ли повреждения корпуса. Записывать Boolean переменную: ")
+    val damegeHull = readLine().toBoolean()
+    print("Текущая численность экипажа: ")
+    val numberOfCrew = readLine()!!.toInt()
+    print("Текущее количество ящиков провизии: ")
+    val boxOfProvisions = readLine()!!.toInt()
+    print("Благоприятные ли метеоусловия. Записывать Boolean переменную: ")
+    val weatherConditions = readLine().toBoolean()
+
+    val seeOdyssey =
+        ((!damegeHull) && (numberOfCrew in 56..69) && (boxOfProvisions > 50)) or
+                ((numberOfCrew == 70) && (boxOfProvisions > 50) && (weatherConditions))
+
+    println("будет ли одисея: $seeOdyssey")
+
+}
