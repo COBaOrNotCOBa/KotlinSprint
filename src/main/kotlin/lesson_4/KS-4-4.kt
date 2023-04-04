c0b9b50dba807141ffd0223dbf9b8e26a3331a87
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-04 10:54:30 +0300

    KS-4-4 04.04.2023 Training day

--- src/main/kotlin/lesson_4/KS-4-4.kt
+++ src/main/kotlin/lesson_4/KS-4-4.kt
@@ -1,2 +1,20 @@
 package lesson_4
 
+fun main() {
+
+    val daysOfTraining : Short = 5
+    val exerciseHand = daysOfTraining % 2 != 0
+    val exerciseLeg = daysOfTraining % 2 == 0
+    val exerciseBack = daysOfTraining % 2 == 0
+    val exercisePress = daysOfTraining % 2 != 0
+    println(
+        """
+        Упражнения для рук:    $exerciseHand
+        Упражнения для ног:    $exerciseLeg
+        Упражнения для спины:  $exerciseBack
+        Упражнения для пресса: $exercisePress
+    """.trimIndent()
+    )
+
+
+}
