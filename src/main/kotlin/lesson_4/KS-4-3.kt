e2168bb7423ad76cd5e39d7a492944774c8c71bc
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-04 11:47:43 +0300

    KS-4-3 04.04.2023 String changed to Boolean

--- src/main/kotlin/lesson_4/KS-4-3.kt
+++ src/main/kotlin/lesson_4/KS-4-3.kt
@@ -2,10 +2,10 @@ package lesson_4
 
 fun main() {
 
-    val weather = "солнечная"
-    val tent = "раскрыт"
-    val humidity = "20"
-    val season = "время года зима"
+    val weather = true
+    val tent = true
+    val humidity = 20f
+    val season = "winter"
     println(
         "Благоприятные ли условия сейчас для роста бобовых? " +
                 "${(weather == LEGUMES_WEATHER) && (tent == LEGUMES_TENT) && (humidity == LEGUMES_HUMIDITY) && (season != LEGUMES_SEASON)}"
@@ -13,7 +13,7 @@ fun main() {
 
 }
 
-const val LEGUMES_WEATHER = "солнечная"
-const val LEGUMES_TENT = "раскрыт"
-const val LEGUMES_HUMIDITY = "20"
-const val LEGUMES_SEASON = "время года зима"
+const val LEGUMES_WEATHER = true
+const val LEGUMES_TENT = true
+const val LEGUMES_HUMIDITY = 20f
+const val LEGUMES_SEASON = "winter"
