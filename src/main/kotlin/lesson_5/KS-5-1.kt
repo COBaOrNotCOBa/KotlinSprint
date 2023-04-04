d87f5b60075f5324f4e80427f1905367e1dc8ab2
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-04 12:31:56 +0300

    KS-5-1 04.04.2023 check access

--- src/main/kotlin/lesson_5/KS-5-1.kt
+++ src/main/kotlin/lesson_5/KS-5-1.kt
@@ -1,2 +1,12 @@
 package lesson_5
 
+fun main() {
+
+    println("Сколько будет 2+2=")
+    val answer = readLine()!!.toInt()
+
+    val access = if (answer == 4) "Добро пожаловать!"
+    else "Доступ запрещен."
+
+    println(access)
+}
