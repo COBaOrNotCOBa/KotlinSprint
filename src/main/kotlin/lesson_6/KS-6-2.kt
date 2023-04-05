170b69a237e2b857232ad327f91a1c8dd5011126
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-05 19:20:03 +0300

    KS-6-2 05.04.2023 Timer seconds

--- src/main/kotlin/lesson_6/KS-6-2.kt
+++ src/main/kotlin/lesson_6/KS-6-2.kt
@@ -1,2 +1,16 @@
 package lesson_6
 
+fun main() {
+
+    println("Введите сколько секунд нужно засечь: ")
+    val seconds = readln().toInt()
+    var counter = seconds
+
+    while (counter > 0) {
+        Thread.sleep(1000)
+        --counter
+    }
+
+    println("Прошло $seconds секунд")
+
+}
