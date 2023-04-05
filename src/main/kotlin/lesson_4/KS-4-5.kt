36ee9ad094ceb53f62085e42978d3ac4a7dee56c
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-05 13:50:07 +0300

    KS-4-5 05.04.2023 Add two boolean variable. Add transfer. or --> || есть не уверенность где стоит оставлять пустые строки, а где нет.

--- src/main/kotlin/lesson_4/KS-4-5.kt
+++ src/main/kotlin/lesson_4/KS-4-5.kt
@@ -2,10 +2,6 @@ package lesson_4
 
 fun main() {
 
-    val minimumNumberCrew = 55
-    val maximumNumberCrew = 70
-    val minimumBoxOfProvision = 50
-
     print("Есть ли повреждения корпуса. Записывать Boolean переменную: ")
     val damegeHull = readln().toBoolean()
     print("Текущая численность экипажа: ")
@@ -15,9 +11,18 @@ fun main() {
     print("Благоприятные ли метеоусловия. Записывать Boolean переменную: ")
     val weatherConditions = readln().toBoolean()
 
-    val seeOdyssey =
-        ((!damegeHull) && (numberCrew > minimumNumberCrew) && (numberCrew < maximumNumberCrew) && (boxOfProvisions > minimumBoxOfProvision)) or
-                ((numberCrew == maximumNumberCrew) && (boxOfProvisions > minimumBoxOfProvision) && (weatherConditions))
+    val minimumNumberCrew = 55
+    val maximumNumberCrew = 70
+    val minimumBoxOfProvision = 50
+    val resultNotDamage = ((!damegeHull) &&
+            (numberCrew > minimumNumberCrew) &&
+            (numberCrew < maximumNumberCrew) &&
+            (boxOfProvisions > minimumBoxOfProvision))
+    val resultGoodWeather = ((numberCrew == maximumNumberCrew) &&
+            (boxOfProvisions > minimumBoxOfProvision) &&
+            (weatherConditions))
+
+    val seeOdyssey = resultNotDamage || resultGoodWeather
 
     println("Будет ли одисея: $seeOdyssey")
 
