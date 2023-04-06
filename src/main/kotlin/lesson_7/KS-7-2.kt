9a5d4d90372355a83d74cab548782cd82dcc65b0
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-06 15:58:31 +0300

    KS-7-2 06.04.2023 Sms code

--- src/main/kotlin/lesson_7/KS-7-2.kt
+++ src/main/kotlin/lesson_7/KS-7-2.kt
@@ -2,9 +2,9 @@ package lesson_7
 
 fun main() {
 
-    val smsCode : IntRange = 1000..9999
-    var currentCode : Int
-    var userCode : Int
+    val smsCode: IntRange = 1000..9999
+    var currentCode: Int
+    var userCode: Int
 
     do {
         currentCode = smsCode.random()
