ee9fcd265c162aca601a93ec1a37e51110d23b66
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-06 16:13:40 +0300

    KS-7-3 06.04.2023 Evens user numbers

--- src/main/kotlin/lesson_7/KS-7-3.kt
+++ src/main/kotlin/lesson_7/KS-7-3.kt
@@ -1,2 +1,10 @@
 package lesson_7
 
+fun main() {
+
+    println("Введите число до которого показать все чётные числа")
+    val userNumber = readln().toInt()
+
+    for (i in 0..userNumber step 2) println(i)
+
+}
