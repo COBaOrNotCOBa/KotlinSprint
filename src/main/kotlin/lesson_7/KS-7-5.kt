cf853b31f25ce0fcedb40bb3fb6fa8cdea69fc17
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-06 16:43:35 +0300

    KS-7-5 06.04.2023 Upgrade password generater

--- src/main/kotlin/lesson_7/KS-7-5.kt
+++ src/main/kotlin/lesson_7/KS-7-5.kt
@@ -1,2 +1,23 @@
 package lesson_7
 
+fun main() {
+
+    val passwordNumber: IntRange = 0..9
+    val passwordCharLower: CharRange = 'a'..'z'
+    val passwordCharCapital: CharRange = 'A'..'Z'
+    var passwordFull = ""
+
+    println("Введите длину генерируемого пароля: ")
+    val passwordLenght = readln().toInt()
+
+    for (i in 1..passwordLenght) {
+        when ((1..3).random()) {
+            1 -> passwordFull += passwordNumber.random()
+            2 -> passwordFull += passwordCharLower.random()
+            3 -> passwordFull += passwordCharCapital.random()
+        }
+    }
+
+    println(passwordFull)
+
+}
