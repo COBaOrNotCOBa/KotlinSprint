4ed755a17fdee1df71a603833e7b06c901853351
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-07 11:08:53 +0300

    KS-8-1 07.04.2023 Summ views per week

--- src/main/kotlin/lesson_8/KS-8-1.kt
+++ src/main/kotlin/lesson_8/KS-8-1.kt
@@ -2,10 +2,10 @@ package lesson_8
 
 fun main() {
 
-    val views = arrayOf(23,34,45,56,67,78,1)
+    val views = arrayOf(23, 34, 45, 56, 67, 78, 1)
     var summViewsPerWeek = 0
 
-    for (i in 0 until views.size) summViewsPerWeek+= views[i]
+    for (i in 0 until views.size) summViewsPerWeek += views[i]
 
     println(summViewsPerWeek)
 }
