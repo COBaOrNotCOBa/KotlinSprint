8e0e68a4b96003e5d124c7ffbf4a17b64b81b830
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-08 11:46:40 +0300

    KS-8-2 08.04.2023 add cycle For

--- src/main/kotlin/lesson_8/KS-8-2.kt
+++ src/main/kotlin/lesson_8/KS-8-2.kt
@@ -3,11 +3,19 @@ package lesson_8
 fun main() {
 
     val ingredients = arrayOf("огурцы", "помидоры", "перец", "соль", "подсолнечное масло")
+    var result = false
 
     println("Введите название искомого ингридиента: ")
     val userIngredient = readln()
 
-    if (ingredients.indexOf(userIngredient) > -1) println("Ингредиент $userIngredient в рецепте есть")
+    for (i in 0 until ingredients.size) {
+        if (userIngredient == ingredients[i]) {
+            result = true
+            break
+        }
+    }
+
+    if (result) println("Ингредиент $userIngredient в рецепте есть")
     else println("Такого ингредиента в рецепте нет")
 
 }
