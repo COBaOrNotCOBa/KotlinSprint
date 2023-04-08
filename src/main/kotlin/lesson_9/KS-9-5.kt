75c28b80ad9bbb64b1e3e2999cfa3d349a718f34
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-08 14:48:09 +0300

    KS-9-5 08.04.2023 in progress

--- src/main/kotlin/lesson_9/KS-9-5.kt
+++ src/main/kotlin/lesson_9/KS-9-5.kt
@@ -1,2 +1,22 @@
 package lesson_9
 
+fun main() {
+
+    val listOfIngredients = mutableListOf<String>()
+    var ingredient: String
+    var ingredientsAll = ""
+    println("Введите по очереди пять ингредиентов: ")
+
+    for (i in 0..4){
+        ingredient = readln()
+        listOfIngredients.add(ingredient)
+    }
+
+    listOfIngredients.sort()
+    for (i in 0..4){
+        ingredientsAll += listOfIngredients[i] + " "
+    }
+
+    println(ingredientsAll)
+
+}
