1a6ff3950cbd10f73d557702d0e41a42f7fdf56c
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-09 18:04:14 +0300

    KS-10-4 09.04.2023 Mini game dice roll in cycle

--- src/main/kotlin/lesson_10/KS-10-4.kt
+++ src/main/kotlin/lesson_10/KS-10-4.kt
@@ -1,2 +1,40 @@
 package lesson_10
 
+fun main() {
+
+    var humanDice: Int
+    var compDice:Int
+    var humanChoice: String
+    var humanWins = 0
+
+    do {
+        println("Твой бросок: ")
+        humanDice = diceRoll()
+        println("Компьютерный бросок")
+        compDice = diceRoll()
+
+    when {
+        humanDice == compDice -> println("Это был равный бой!")
+        humanDice > compDice -> {
+            println("Победило человечество")
+            humanWins++
+        }
+        humanDice < compDice -> println("Победила машина")
+    }
+
+        println("Хотите бросить кости еще раз Введите Да или Нет: ")
+        humanChoice = readln()
+
+    } while (humanChoice=="Да")
+
+    println("Количество побед у человечества: $humanWins")
+
+}
+
+fun diceRoll(): Int {
+    val firstDice = (1..6).random()
+    println("На первом кубике: $firstDice")
+    val secondDice = (1..6).random()
+    println("На втором кубике: $secondDice")
+    return firstDice + secondDice
+}
