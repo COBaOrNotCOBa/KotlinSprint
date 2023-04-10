ce185b545be07fe9588df99b86f3bbadabb51944
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-10 21:43:13 +0300

    KS-11-4 09.04.2023 Contakt class опять у меня ощущение что, что-то не так

---
+++ src/main/kotlin/lesson_11/KS-11-4.kt
@@ -0,0 +1,14 @@
+package lesson_11
+
+fun main() {
+
+    val contakt = Contakt(
+        "Mouse Face",
+        "First Name",
+        "Second Name",
+        "8 (999) 999-42-42",
+        "8 (999) 888-42-42",
+        "mail@mail.ru"
+    )
+
+}
