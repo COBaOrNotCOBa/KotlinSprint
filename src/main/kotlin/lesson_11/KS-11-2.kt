230a0cbac060d74a9358e8405ff0cfba222f5f74
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-11 10:01:26 +0300

    KS-11-2 11.04.2023 merged files

--- src/main/kotlin/lesson_11/KS-11-2.kt
+++ src/main/kotlin/lesson_11/KS-11-2.kt
@@ -15,3 +15,36 @@ fun main() {
     user.textToSendUser("kipasa muchacho")
 
 }
+
+class UserAdvanced(
+    val id: Int,
+    val login: String,
+    var password: String,
+    val eMail: String,
+    var bio: String? = null
+) {
+    fun userInformation() {
+        println("id: $id")
+        println("login: $login")
+        println("password: $password")
+        println("email: $eMail")
+        if (bio != "") println("О себе: $bio")
+    }
+
+    fun writeBio() {
+        println("Введите информацио о себе")
+        bio = readln()
+    }
+
+    fun passwordChanging() {
+        println("Введите текущий пароль: ")
+        if (password == readln()) {
+            println("Введите новый пароль: ")
+            password = readln()
+            println("Пароль успешно изменён")
+        } else println("Введён неверный пароль")
+    }
+
+    fun textToSendUser(text: String) = println("Уважаемый $login вам сообщение: \"$text\" отправлено на почту $eMail")
+
+}
