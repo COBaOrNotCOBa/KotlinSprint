2e8da31745e8dd3fe60f77719c5e00107ef62bda
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-11 10:05:53 +0300

    KS-11-3 11.04.2023 merged files

--- src/main/kotlin/lesson_11/KS-11-3.kt
+++ src/main/kotlin/lesson_11/KS-11-3.kt
@@ -26,3 +26,25 @@ fun main() {
 
     roomChatTwo.longPushAvatar()
 }
+
+class RoomChat(
+    val cover: String,
+    val roomName: String,
+    val listOfParticipants: List<String>,
+) {
+    val statusList = listOf("разговаривает", "микрофон выключен", "пользователь заглушен")
+
+    fun longPushAvatar() {
+        println()
+        println(listOfParticipants[0])
+    }
+
+    fun displeyRoom() {
+        println()
+        println("Обложка комнаты: $cover")
+        println("Название комнаты: $roomName")
+        for (i in 0 until listOfParticipants.size) {
+            println("Аватар пользователя ${i + 1} в состояние: ${statusList.random()}")
+        }
+    }
+}
