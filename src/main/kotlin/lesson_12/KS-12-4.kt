b2ce81655c3de11009e6016f07bd128510e4a387
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-12 15:49:50 +0300

    KS-12-3 12.04.2023 Init Days condition

---
+++ src/main/kotlin/lesson_12/KS-12-4.kt
@@ -0,0 +1,39 @@
+package lesson_12
+
+fun main() {
+
+    val dayOne = Day(
+        12,
+        15,
+        800,
+        true,
+    )
+    val dayTwo = Day(
+        42,
+        -15,
+        900,
+    )
+    val dayThree = Day(
+        35,
+        15,
+        760,
+    )
+
+}
+
+class Day(
+    temperatureDay: Int,
+    temperatureNight: Int,
+    atmosphericPressure: Int,
+    rain: Boolean = false,
+) {
+
+    init {
+        println(
+            "Температура днём: $temperatureDay \n" +
+                    "температура ночью: $temperatureNight \n" +
+                    "Атмосферное давление: $atmosphericPressure \n" +
+                    "Был дождь: $rain \n"
+        )
+    }
+}
