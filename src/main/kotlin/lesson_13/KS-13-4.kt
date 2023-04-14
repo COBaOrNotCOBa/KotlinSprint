6058d8bbb8c5496bdda826577198b384e59592a8
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-14 13:19:04 +0300

    KS-13-4 13.04.2023 + readlnOrNull()?.takeIf { it.isNotEmpty() } ?: "Не указано"

--- src/main/kotlin/lesson_13/KS-13-4.kt
+++ src/main/kotlin/lesson_13/KS-13-4.kt
@@ -28,15 +28,15 @@ class PhoneBookFunction {
 
         do {
             println("Введите имя: ")
-            val name = readLine()
+            val name = readlnOrNull()?.takeIf { it.isNotEmpty() } ?: "Не указано"
             println("Введите номер телефона: ")
-            val telephoneNumber = readLine()
+            val telephoneNumber = readlnOrNull()?.takeIf { it.isNotEmpty() } ?: "Не указано"
             println("Введите название компании: ")
-            val company = readLine()
-            if (telephoneNumber != null && (name != null || company != null)) {
+            val company = readlnOrNull()?.takeIf { it.isNotEmpty() } ?: "Не указано"
+            if (telephoneNumber != "Не указано" && (name != "Не указано" || company != "Не указано")) {
                 val contact = PhoneBook4(name, telephoneNumber, company)
                 listOfContact4.add(contact)
-            } else println("Контакт небыл добавлен")
+            } else println("Контакт небыл добавлен \n")
             println("Если хотите добавить новую запись, введите \"да\"")
         } while (readLine() == "да")
 
