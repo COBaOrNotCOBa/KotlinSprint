981056d6f61b9d4b1820b2f2332542ce20dcca30
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-15 19:31:21 +0300

    KS-15-1 interface movement

--- src/main/kotlin/lesson_15/KS-15-1.kt
+++ src/main/kotlin/lesson_15/KS-15-1.kt
@@ -1,2 +1,49 @@
 package lesson_15
 
+fun main() {
+
+    val carp = CrucianCarp()
+    val seagull = Seagull()
+    val duck = Duck()
+
+    carp.movementOnWater()
+    seagull.movementOnWater()
+    seagull.movementOnAir()
+    duck.movementOnWater()
+    duck.movementOnAir()
+
+}
+
+interface movementOnWater {
+    fun movementOnWater()
+}
+
+interface movementOnAir {
+    fun movementOnAir()
+}
+
+class CrucianCarp() : movementOnWater {
+    override fun movementOnWater() {
+        println("Карп плывёт")
+    }
+}
+
+class Seagull() : movementOnWater, movementOnAir {
+    override fun movementOnWater() {
+        println("Чайка плывет")
+    }
+
+    override fun movementOnAir() {
+        println("Чайка летит")
+    }
+}
+
+class Duck() : movementOnWater, movementOnAir {
+    override fun movementOnWater() {
+        println("Утка плавет")
+    }
+
+    override fun movementOnAir() {
+        println("Утка летит")
+    }
+}
