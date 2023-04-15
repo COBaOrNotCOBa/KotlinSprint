dfb22d561854417850358e8b517ec7a467e710a5
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-15 20:00:50 +0300

    KS-15-2 15.04.2023 Weather station

--- src/main/kotlin/lesson_15/KS-15-2.kt
+++ src/main/kotlin/lesson_15/KS-15-2.kt
@@ -1,2 +1,34 @@
 package lesson_15
 
+fun main() {
+
+    val temperature = TemperatureStationOptions()
+    val rainfall = Rainfall()
+
+    temperature.connectToServer()
+    temperature.sendDataToServer()
+    temperature.temperatureData()
+
+    rainfall.connectToServer()
+    rainfall.sendDataToServer()
+    rainfall.rainfallData()
+
+}
+
+abstract class WeatherStationOptions() {
+
+    fun connectToServer() = println("Соединение с сервером установленно")
+
+    fun sendDataToServer() = println("Данные отправленны на сервер")
+}
+
+class TemperatureStationOptions() : WeatherStationOptions() {
+
+    fun temperatureData() = println("Текущая температура")
+}
+
+class Rainfall() : WeatherStationOptions() {
+
+    fun rainfallData() = println("Текущее количество осадков")
+}
+
