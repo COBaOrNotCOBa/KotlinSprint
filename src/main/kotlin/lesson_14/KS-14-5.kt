e37c6d3c6347bffc3fe35e26373d5882a0208a16
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-15 14:16:56 +0300

    KS-14-5 15.04.2023 Red Figures

---
+++ src/main/kotlin/lesson_14/KS-14-5.kt
@@ -0,0 +1,109 @@
+package lesson_14
+
+
+fun main() {
+
+    val listOfFigeures = mutableListOf<Figure>()
+
+    val circleOne = Circle(
+        "Red",
+        15.0,
+    )
+    val circleTwo = Circle(
+        "Black",
+        5.0,
+    )
+    val rectangleOne = Rectangle(
+        "Red",
+        5.0,
+        10.0,
+    )
+    val rectangleTwo = Rectangle(
+        "Green",
+        5.0,
+        10.0,
+    )
+    val triangleOne = Triangle(
+        "Black",
+        5.0,
+        4.0,
+        3.0,
+        2.4,
+    )
+    val triangleTwo = Triangle(
+        "Green",
+        10.0,
+        8.0,
+        6.0,
+        4.8,
+    )
+
+    listOfFigeures.add(circleOne)
+    listOfFigeures.add(circleTwo)
+    listOfFigeures.add(rectangleOne)
+    listOfFigeures.add(rectangleTwo)
+    listOfFigeures.add(triangleOne)
+    listOfFigeures.add(triangleTwo)
+
+    circleOne.sumOfPerimetersRedFigures(listOfFigeures)
+    circleOne.sumOfSquareRedFigures(listOfFigeures)
+}
+
+abstract class Figure(val color: String) {
+
+    abstract fun squareFigure() :Double
+
+    abstract fun perimeterFigure() :Double
+
+    fun sumOfPerimetersRedFigures(listOfFigures : List<Figure>){
+        var sum = 0.0
+        val redsFigures = listOfFigures.find {it.color == "Red"}
+        listOfFigures.forEach{
+            if (it.color=="Red") sum += it.perimeterFigure()
+        }
+        println("Сумма периметров красных фигур: $sum")
+    }
+
+    fun sumOfSquareRedFigures(listOfFigures : List<Figure>){
+        var sum = 0.0
+        val redsFigures = listOfFigures.find {it.color == "Red"}
+        listOfFigures.forEach{
+            if (it.color=="Red") sum += it.squareFigure()
+        }
+        println("Сумма площадей красных фигур: $sum")
+    }
+}
+
+class Circle(
+    color: String,
+    val radius: Double,
+) : Figure(color) {
+
+    override fun squareFigure() = 3.14 * radius * radius
+
+    override fun perimeterFigure() = 2 * 3.14 * radius
+}
+
+class Rectangle(
+    color: String,
+    val length: Double,
+    val width: Double,
+) : Figure(color) {
+
+    override fun squareFigure() =length * width
+
+    override fun perimeterFigure() = 2 * (length + width)
+}
+
+class Triangle(
+    color: String,
+    val sideOne: Double,
+    val sideTwo: Double,
+    val sideThree: Double,
+    val heigth: Double,
+) : Figure(color) {
+
+    override fun squareFigure() = 0.5 * sideOne * heigth
+
+    override fun perimeterFigure() = sideOne + sideTwo + sideThree
+}
