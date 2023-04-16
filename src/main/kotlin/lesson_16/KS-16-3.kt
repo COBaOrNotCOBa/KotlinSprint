b2ced077cbf62edd8be766c3acd55a596940f328
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-16 18:17:19 +0300

    KS-16-3 16.04.2023 Private user password

--- src/main/kotlin/lesson_16/KS-16-3.kt
+++ src/main/kotlin/lesson_16/KS-16-3.kt
@@ -1,2 +1,20 @@
 package lesson_16
 
+fun main() {
+
+    val user = User16_3(
+        "Bongo",
+        "123",)
+
+    user.checkPassword("345")
+}
+
+class User16_3(
+    val login: String,
+    private val password: String,
+) {
+    fun checkPassword(inputPassword: String) {
+        if (inputPassword==password) println("Password is correct")
+        else println("Password is not correct")
+    }
+}
