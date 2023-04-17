6fb9a0648204089a064aa00163e03e66e14577ca
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-17 16:12:02 +0300

    KS-17-3 17.04.2023 Secret folder не уверен так ли, название и количество файлов даю при создании объекта и дальше выводит информацию в зависимости от секретной галки показывает информацию. Правильно ли сделал что в конструктор добавил переменные или можно было оставить всё данные в геттере

--- src/main/kotlin/lesson_17/KS-17-3.kt
+++ src/main/kotlin/lesson_17/KS-17-3.kt
@@ -1,2 +1,32 @@
 package lesson_17
 
+fun main() {
+
+    val folder = Folder17_3(
+        "F",
+        3,
+    )
+    println("Имя: ${folder.name}, количество файлов: ${folder.fileCounter}, секретный флаг: ${folder.flagSecret}")
+    folder.flagSecret = true
+    println("Имя: ${folder.name}, количество файлов: ${folder.fileCounter}, секретный флаг: ${folder.flagSecret}")
+    folder.flagSecret = false
+    println("Имя: ${folder.name}, количество файлов: ${folder.fileCounter}, секретный флаг: ${folder.flagSecret}")
+}
+
+class Folder17_3(
+    val nameFile: String,
+    val counterFile: Int,
+) {
+    val name: String
+        get() {
+            if (flagSecret) return "Скрытая папка"
+            else return nameFile
+        }
+    val fileCounter: Int
+        get() {
+            if (flagSecret) return 0
+            else return counterFile
+        }
+    var flagSecret: Boolean = false
+
+}
