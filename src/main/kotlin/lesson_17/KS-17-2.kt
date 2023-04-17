789b87e2cc97fe21fdf70e6b2cb991e495b5ffd7
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-17 13:51:14 +0300

    KS-17-2 17.04.2023 const ship name и снова сомнения, а так ли с сделал...

--- src/main/kotlin/lesson_17/KS-17-2.kt
+++ src/main/kotlin/lesson_17/KS-17-2.kt
@@ -1,2 +1,20 @@
 package lesson_17
 
+fun main() {
+
+    val ship = Ship17_2()
+    println(ship.name)
+    ship.name = "JustShip"
+    println(ship.name)
+
+}
+
+class Ship17_2 {
+    var name: String = "StarShip"
+        get() = field
+        set(value) {
+            field = name
+        }
+    val averageSpeed: Int = 500
+    val homePort: String = "Haiti"
+}
