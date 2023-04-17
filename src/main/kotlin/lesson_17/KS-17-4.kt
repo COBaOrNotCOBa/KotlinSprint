ed67f1c80a8749847855c7c6472d20b295dca0a4
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-17 17:06:03 +0300

    KS-17-4 17.04.2023 in progress

--- src/main/kotlin/lesson_17/KS-17-4.kt
+++ src/main/kotlin/lesson_17/KS-17-4.kt
@@ -1,2 +1,22 @@
 package lesson_17
 
+fun main() {
+
+    val package
+
+}
+
+class Package17_4() {
+    var packageCounterTranspoted = 0
+    var packageNumber: Int = 1
+        get() = field
+        set(value) {
+            field=1
+        }
+    var packageLocation: String = "Start: $packageCounterTranspoted"
+        get() = field
+        set(value) {
+
+        }
+
+}
