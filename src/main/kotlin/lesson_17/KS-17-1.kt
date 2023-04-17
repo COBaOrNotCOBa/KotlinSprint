f0f18240c2fb35565ae1aee61d90083651caebc1
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-17 12:52:24 +0300

    KS-17-1 17.04.2023 QuizElement get set

--- src/main/kotlin/lesson_17/KS-17-1.kt
+++ src/main/kotlin/lesson_17/KS-17-1.kt
@@ -1,2 +1,20 @@
 package lesson_17
 
+fun main() {
+
+    val firstQuestion = QuizElement()
+    println(firstQuestion.question)
+    println(firstQuestion.answer)
+
+}
+
+class QuizElement {
+    var question = "кот"
+        get() = "геттервопрос"
+    var answer = "котейка"
+        get() = "геттерответ"
+        set(value) {
+            field = value
+        }
+
+}
