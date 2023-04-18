1016a885bb1c1afe3b0a63718a94d3451c2ea012
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-18 15:14:38 +0300

    KS-18-3 18.04.2023 Tamagochi сделал как-то недумая, а выглядит хорошо )

--- src/main/kotlin/lesson_18/KS-18-3.kt
+++ src/main/kotlin/lesson_18/KS-18-3.kt
@@ -1,2 +1,25 @@
 package lesson_18
 
+fun main() {
+
+    val fox = Fox("Алиса")
+    val dog = Dog("Скуби")
+    val cat = Cat("Зуксель")
+
+    fox.animalEating()
+    dog.animalEating()
+    cat.animalEating()
+    fox.animalPlaing()
+    cat.animalSleep()
+
+}
+
+open class TamogochiAnimal(val name: String,val eat : String) {
+    fun animalEating() = println("$name ест $eat")
+    fun animalSleep() = println("$name спит")
+    fun animalPlaing() = println("$name играет")
+}
+
+class Fox(name: String) : TamogochiAnimal(name, eat = "ягоды")
+class Dog(name: String) : TamogochiAnimal(name, eat = "кости")
+class Cat(name: String) : TamogochiAnimal(name, eat = "рыбу")
