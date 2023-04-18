6297b9d82f3b8d4462c4d307e138bbcfc4f4fa63
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-18 15:42:12 +0300

    KS-18-4 18.04.2023 Package square а так ли я сделал? Или нужно было прямо классы создавать? И конструктор класса стороны вписывать...

--- src/main/kotlin/lesson_18/KS-18-4.kt
+++ src/main/kotlin/lesson_18/KS-18-4.kt
@@ -1,2 +1,24 @@
 package lesson_18
 
+fun main() {
+
+    Package18_4().squareOfFigure(5.0)
+    Package18_4().squareOfFigure(listOf(2.0, 3.0, 5.0))
+
+}
+
+class Package18_4() {
+    fun squareOfFigure(
+        length: Double,
+    ) {
+        println("Площадь куба равна: ${6 * length * length}")
+    }
+    fun squareOfFigure(
+        length: List<Double>,
+    ) {
+        println(
+            "Площадь параллелепипеда равна: " +
+                    "${2 * (length[0] * length[1] + length[1] * length[2] + length[2] * length[0])}"
+        )
+    }
+}
