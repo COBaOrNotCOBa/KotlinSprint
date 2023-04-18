1710aa3625f8d4bae97990a19e43fcff417690c7
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-18 16:58:10 +0300

    KS-18-5 18.04.2023 Screen and draw() а туда ли я копаю...

--- src/main/kotlin/lesson_18/KS-18-5.kt
+++ src/main/kotlin/lesson_18/KS-18-5.kt
@@ -1,2 +1,48 @@
 package lesson_18
 
+fun main() {
+    val screen = Screen()
+    val point = Point(10, 20.5f)
+    val square = Square(100.5f, 200)
+    val circle = Circle(300, 400.5)
+
+    screen.draw(point)
+    screen.draw(square)
+    screen.draw(circle)
+}
+
+abstract class Shape(
+    val x: Number,
+    val y: Number,
+    val shape: String,
+)
+
+class Point(
+    x: Number,
+    y: Number,
+) : Shape(x, y, shape = "точку")
+
+class Square(
+    x: Number,
+    y: Number,
+) : Shape(x, y, shape = "квадрат")
+
+class Circle(
+    x: Number,
+    y: Number,
+) : Shape(x, y, shape = "круг")
+
+class Screen {
+
+    fun draw(point: Point) {
+        println("Рисуем ${point.shape} ${point.x} ${point.y}")
+    }
+
+    fun draw(square: Square) {
+        println("Рисуем ${square.shape} ${square.x} ${square.y}")
+    }
+
+    fun draw(circle: Circle) {
+        println("Рисуем ${circle.shape} ${circle.x} ${circle.y}")
+    }
+}
