d4e383c042cc23478ff93bdd1ba94f8a27024e8a
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-18 11:37:31 +0300

    KS-17-4 18.04.2023 Package location

--- src/main/kotlin/lesson_17/KS-17-4.kt
+++ src/main/kotlin/lesson_17/KS-17-4.kt
@@ -2,21 +2,33 @@ package lesson_17
 
 fun main() {
 
-    val package
+    val myPackage = Package17_4(2)
 
+    println(
+        "Номер посылки ${myPackage.packageNumber}," +
+                " Количество перемещений ${myPackage.packageCounterTranspoted}," +
+                " Место нахождениря ${myPackage.packageLocation}"
+    )
+    myPackage.packageLocation = "B"
+    println(
+        "Номер посылки ${myPackage.packageNumber}," +
+                " Количество перемещений ${myPackage.packageCounterTranspoted}," +
+                " Место нахождениря ${myPackage.packageLocation}"
+    )
+    myPackage.packageLocation = "C"
+    println(
+        "Номер посылки ${myPackage.packageNumber}," +
+                " Количество перемещений ${myPackage.packageCounterTranspoted}," +
+                " Место нахождениря ${myPackage.packageLocation}"
+    )
 }
 
-class Package17_4() {
+class Package17_4(val packageNumber: Int) {
     var packageCounterTranspoted = 0
-    var packageNumber: Int = 1
-        get() = field
+    var packageLocation: String = "A"
         set(value) {
-            field=1
-        }
-    var packageLocation: String = "Start: $packageCounterTranspoted"
-        get() = field
-        set(value) {
-
+            if (field != value) packageCounterTranspoted++
+            field = value
         }
 
 }
