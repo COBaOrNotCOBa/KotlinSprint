bdeda4c6cf8dd2863ee5f64332f33af773929c18
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-20 21:40:14 +0300

    KS-19-1 20.04.2023 List of fishes for aquarium а не многоли я всего написал...

--- src/main/kotlin/lesson_19/KS-19-1.kt
+++ src/main/kotlin/lesson_19/KS-19-1.kt
@@ -1,2 +1,31 @@
 package lesson_19
 
+fun main() {
+
+    val listOfFishes = listOf(1,2,3,4)
+
+    for (i in listOfFishes) {
+        when(i){
+            Fishes19_1.GUPPY.id-> fishName19_1(Fishes19_1.GUPPY)
+            Fishes19_1.ANGELFISH.id-> fishName19_1(Fishes19_1.ANGELFISH)
+            Fishes19_1.GOLDFISH.id-> fishName19_1(Fishes19_1.GOLDFISH)
+            Fishes19_1.SIAMESEFIGHTINGFISH.id-> fishName19_1(Fishes19_1.SIAMESEFIGHTINGFISH)
+        }
+    }
+}
+
+enum class Fishes19_1(val id :Int) {
+    GUPPY(1),
+    ANGELFISH(2),
+    GOLDFISH(3),
+    SIAMESEFIGHTINGFISH(4),
+}
+
+fun fishName19_1(fishName:Fishes19_1){
+    when(fishName){
+        Fishes19_1.GUPPY -> println("Вы можете поместить в свой аквариум рыбу гуппи")
+        Fishes19_1.ANGELFISH -> println("Вы можете поместить в свой аквариум рыбу скалярия")
+        Fishes19_1.GOLDFISH ->println("Вы можете поместить в свой аквариум золотую рыбку")
+        Fishes19_1.SIAMESEFIGHTINGFISH->println("Вы можете поместить в свой аквариум рыбу петушок")
+    }
+}
