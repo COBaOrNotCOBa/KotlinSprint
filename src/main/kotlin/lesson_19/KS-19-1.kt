44d01a4022b2cc5c9416873ca7b65d3899e0ece9
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-21 12:26:55 +0300

    KS-19-1 21.04.2023

--- src/main/kotlin/lesson_19/KS-19-1.kt
+++ src/main/kotlin/lesson_19/KS-19-1.kt
@@ -9,7 +9,7 @@ fun main() {
             Fishes19_1.GUPPY.id-> fishName19_1(Fishes19_1.GUPPY)
             Fishes19_1.ANGELFISH.id-> fishName19_1(Fishes19_1.ANGELFISH)
             Fishes19_1.GOLDFISH.id-> fishName19_1(Fishes19_1.GOLDFISH)
-            Fishes19_1.SIAMESEFIGHTINGFISH.id-> fishName19_1(Fishes19_1.SIAMESEFIGHTINGFISH)
+            Fishes19_1.SIAMESE_FIGHTING_FISH.id-> fishName19_1(Fishes19_1.SIAMESE_FIGHTING_FISH)
         }
     }
 }
@@ -18,7 +18,7 @@ enum class Fishes19_1(val id :Int) {
     GUPPY(1),
     ANGELFISH(2),
     GOLDFISH(3),
-    SIAMESEFIGHTINGFISH(4),
+    SIAMESE_FIGHTING_FISH(4),
 }
 
 fun fishName19_1(fishName:Fishes19_1){
@@ -26,6 +26,6 @@ fun fishName19_1(fishName:Fishes19_1){
         Fishes19_1.GUPPY -> println("Вы можете поместить в свой аквариум рыбу гуппи")
         Fishes19_1.ANGELFISH -> println("Вы можете поместить в свой аквариум рыбу скалярия")
         Fishes19_1.GOLDFISH ->println("Вы можете поместить в свой аквариум золотую рыбку")
-        Fishes19_1.SIAMESEFIGHTINGFISH->println("Вы можете поместить в свой аквариум рыбу петушок")
+        Fishes19_1.SIAMESE_FIGHTING_FISH->println("Вы можете поместить в свой аквариум рыбу петушок")
     }
 }
