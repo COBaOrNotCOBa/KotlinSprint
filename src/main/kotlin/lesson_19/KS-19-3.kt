de4697352f62d54877f87c224fe2c2fd2d03ce55
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-21 12:58:57 +0300

    KS-19-3 21.04.2023 Status SpaceShip TODO а так ли я понял задание...

--- src/main/kotlin/lesson_19/KS-19-3.kt
+++ src/main/kotlin/lesson_19/KS-19-3.kt
@@ -1,2 +1,29 @@
 package lesson_19
 
+fun main() {
+
+    println(Status19_3.TAKE_OFF.getStatus())
+    println(Status19_3.LANDING.getStatus())
+//    println(Status19_3.SHOOTING_ASTEROIDS.getStatus())
+
+}
+
+enum class Status19_3(val id: Int) {
+    TAKE_OFF(101){
+        override fun getStatus(): String {
+            return "Взлетает"
+        }
+                 },
+    LANDING(102){
+        override fun getStatus(): String {
+           return "need more information for logic"
+        }
+                },
+    SHOOTING_ASTEROIDS(103){
+        override fun getStatus(): String {
+            TODO("NotImplementedError")
+        }
+    };
+
+    abstract fun getStatus() : String
+}
