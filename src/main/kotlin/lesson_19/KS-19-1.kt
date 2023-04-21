92af9b32021e686bb096702d1df0e401dd1181f1
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-21 19:34:10 +0300

    KS-19-1 21.04.2023 вроде включил автоформатирование...

--- src/main/kotlin/lesson_19/KS-19-1.kt
+++ src/main/kotlin/lesson_19/KS-19-1.kt
@@ -2,30 +2,30 @@ package lesson_19
 
 fun main() {
 
-    val listOfFishes = listOf(1,2,3,4)
+    val listOfFishes = listOf(1, 2, 3, 4)
 
     for (i in listOfFishes) {
-        when(i){
-            Fishes19_1.GUPPY.id-> fishName19_1(Fishes19_1.GUPPY)
-            Fishes19_1.ANGELFISH.id-> fishName19_1(Fishes19_1.ANGELFISH)
-            Fishes19_1.GOLDFISH.id-> fishName19_1(Fishes19_1.GOLDFISH)
-            Fishes19_1.SIAMESE_FIGHTING_FISH.id-> fishName19_1(Fishes19_1.SIAMESE_FIGHTING_FISH)
+        when (i) {
+            Fishes19_1.GUPPY.id -> fishName19_1(Fishes19_1.GUPPY)
+            Fishes19_1.ANGELFISH.id -> fishName19_1(Fishes19_1.ANGELFISH)
+            Fishes19_1.GOLDFISH.id -> fishName19_1(Fishes19_1.GOLDFISH)
+            Fishes19_1.SIAMESE_FIGHTING_FISH.id -> fishName19_1(Fishes19_1.SIAMESE_FIGHTING_FISH)
         }
     }
 }
 
-enum class Fishes19_1(val id :Int) {
+enum class Fishes19_1(val id: Int) {
     GUPPY(1),
     ANGELFISH(2),
     GOLDFISH(3),
     SIAMESE_FIGHTING_FISH(4),
 }
 
-fun fishName19_1(fishName:Fishes19_1){
-    when(fishName){
+fun fishName19_1(fishName: Fishes19_1) {
+    when (fishName) {
         Fishes19_1.GUPPY -> println("Вы можете поместить в свой аквариум рыбу гуппи")
         Fishes19_1.ANGELFISH -> println("Вы можете поместить в свой аквариум рыбу скалярия")
-        Fishes19_1.GOLDFISH ->println("Вы можете поместить в свой аквариум золотую рыбку")
-        Fishes19_1.SIAMESE_FIGHTING_FISH->println("Вы можете поместить в свой аквариум рыбу петушок")
+        Fishes19_1.GOLDFISH -> println("Вы можете поместить в свой аквариум золотую рыбку")
+        Fishes19_1.SIAMESE_FIGHTING_FISH -> println("Вы можете поместить в свой аквариум рыбу петушок")
     }
 }
