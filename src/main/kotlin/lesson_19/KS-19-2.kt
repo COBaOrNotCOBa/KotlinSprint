039c1a0e08669207ee83d69e4f44f6cf123dc6b2
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-21 12:24:19 +0300

    KS-19-2 21.04.2023 Category of goods в одно строку не смог сделать вывод информации, он всегда в начало пихал результат выполнения метода setTitleGoods, а потом уже весь текст...

--- src/main/kotlin/lesson_19/KS-19-2.kt
+++ src/main/kotlin/lesson_19/KS-19-2.kt
@@ -1,2 +1,41 @@
 package lesson_19
 
+fun main() {
+
+    val goodsOne = GoodsDescription("штаны", 101, Goods19_2.CLOTHES)
+    val goodsTwo = GoodsDescription("ручка", 102, Goods19_2.OFFICE_SUPPLIES)
+    val goodsThree = GoodsDescription("жвачка", 103, Goods19_2.OTHER)
+
+    goodsOne.getInformationOfGoods()
+    goodsTwo.getInformationOfGoods()
+    goodsThree.getInformationOfGoods()
+
+}
+
+class GoodsDescription(val goodsTitle: String, val id: Int, val category: Goods19_2) {
+
+    fun getInformationOfGoods() {
+        when (id) {
+            category.id -> {
+                print("Товар: $goodsTitle, id: ${category.id}, категория: ")
+                setTitleGoods(category)
+            }
+        }
+    }
+}
+
+enum class Goods19_2(val id: Int) {
+    CLOTHES(101),
+    OFFICE_SUPPLIES(102),
+    OTHER(103),
+    ERROR(0),
+}
+
+fun setTitleGoods(goods: Goods19_2) {
+    when (goods) {
+        Goods19_2.CLOTHES -> println("Одежда")
+        Goods19_2.OFFICE_SUPPLIES -> println("Офисные принадлежности")
+        Goods19_2.OTHER -> println("Разное")
+        Goods19_2.ERROR -> println("Не опознано")
+    }
+}
