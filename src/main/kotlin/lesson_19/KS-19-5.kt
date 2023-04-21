50e7e9528c9a0a948f6cb871f7882e511936efc1
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-21 20:39:39 +0300

    KS-19-5 21.04.2023 шаманю...

--- src/main/kotlin/lesson_19/KS-19-5.kt
+++ src/main/kotlin/lesson_19/KS-19-5.kt
@@ -16,10 +16,11 @@ fun main() {
         when (manSex) {
             Sex19_5.MALE.sex -> listOfMans.add(Man19_5(manName, Sex19_5.MALE))
             Sex19_5.FEMALE.sex -> listOfMans.add(Man19_5(manName, Sex19_5.FEMALE))
+            manSex -> listOfMans.add(Man19_5(manName, Sex19_5.ERROR))
         }
     }
 
-    for (i in 0..counterOfMans){
+    for (i in 0..counterOfMans) {
         println("Имя ${listOfMans[i].name}, пол ${listOfMans[i].sex.sex} ")
     }
 }
@@ -29,5 +30,6 @@ class Man19_5(val name: String, val sex: Sex19_5)
 
 enum class Sex19_5(val sex: String) {
     MALE("м"),
-    FEMALE("ж")
+    FEMALE("ж"),
+    ERROR("unknown"),
 }
