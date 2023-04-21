1d62ef9c6d30d660539f76aa2f0ade86ed2a7452
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-21 16:05:50 +0300

    KS-19-5 21.04.2023 File cabinet пока что такой вариант кину, а так думаю что как минимум добавить метод по добавлению человека в класс Man19_5

--- src/main/kotlin/lesson_19/KS-19-5.kt
+++ src/main/kotlin/lesson_19/KS-19-5.kt
@@ -1,2 +1,33 @@
 package lesson_19
 
+fun main() {
+
+    val listOfMans = mutableListOf<Man19_5>()
+    val counterOfMans = 4
+    var manName: String
+    var manSex: String
+
+
+    for (i in 0..counterOfMans) {
+        println("Введите имя человека №${i + 1}: ")
+        manName = readln()
+        println("Введите пол человека №${i + 1}, буквой \"м\" или \"ж\": ")
+        manSex = readln()
+        when (manSex) {
+            Sex19_5.MALE.sex -> listOfMans.add(Man19_5(manName, Sex19_5.MALE))
+            Sex19_5.FEMALE.sex -> listOfMans.add(Man19_5(manName, Sex19_5.FEMALE))
+        }
+    }
+
+    for (i in 0..counterOfMans){
+        println("Имя ${listOfMans[i].name}, пол ${listOfMans[i].sex.sex} ")
+    }
+}
+
+class Man19_5(val name: String, val sex: Sex19_5)
+
+
+enum class Sex19_5(val sex: String) {
+    MALE("м"),
+    FEMALE("ж")
+}
