b115fc04bcc1a31f4159dc832dcb43d92791e33d
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-22 18:37:44 +0300

    KS-19-5 22.04.2023 почистил main :) добавил два метода для добавления людей и вывода в консоль
    также сделал не жетско 5 человека а с выбором когда закончить...

--- src/main/kotlin/lesson_19/KS-19-5.kt
+++ src/main/kotlin/lesson_19/KS-19-5.kt
@@ -2,34 +2,43 @@ package lesson_19
 
 fun main() {
 
-    val listOfMans = mutableListOf<Man19_5>()
-    val counterOfMans = 4
-    var manName: String
-    var manSex: String
+    printMan(addMan())
 
-
-    for (i in 0..counterOfMans) {
-        println("Введите имя человека №${i + 1}: ")
-        manName = readln()
-        println("Введите пол человека №${i + 1}, буквой \"м\" или \"ж\": ")
-        manSex = readln()
-        when (manSex) {
-            Sex19_5.MALE.sex -> listOfMans.add(Man19_5(manName, Sex19_5.MALE))
-            Sex19_5.FEMALE.sex -> listOfMans.add(Man19_5(manName, Sex19_5.FEMALE))
-            manSex -> listOfMans.add(Man19_5(manName, Sex19_5.ERROR))
-        }
-    }
-
-    for (i in 0..counterOfMans) {
-        println("Имя ${listOfMans[i].name}, пол ${listOfMans[i].sex.sex} ")
-    }
 }
 
 class Man19_5(val name: String, val sex: Sex19_5)
 
+fun addMan(): List<Man19_5> {
+    var manName: String
+    var manSex: String
+    val listOfMans = mutableListOf<Man19_5>()
+    var choice: String
+    do {
+        println("Введите имя человека: ")
+        manName = readln()
+        println("Введите пол человека, буквой \"М\" или \"Ж\": ")
+        manSex = readln()
+        listOfMans.add(
+            when (manSex) {
+                Sex19_5.MALE.sex -> Man19_5(manName, Sex19_5.MALE)
+                Sex19_5.FEMALE.sex -> Man19_5(manName, Sex19_5.FEMALE)
+                else -> Man19_5(manName, Sex19_5.ERROR)
+            }
+        )
+        println("Ввести ещё одного человека?")
+        choice = readln()
+    } while (choice == "да")
+    return listOfMans
+}
 
-enum class Sex19_5(val sex: String) {
-    MALE("м"),
-    FEMALE("ж"),
-    ERROR("unknown"),
+fun printMan(listOfMans: List<Man19_5>) {
+    for (i in 0 until listOfMans.size) {
+        println("Имя ${listOfMans[i].name}, пол ${listOfMans[i].sex.sexFull} ")
+    }
+}
+
+enum class Sex19_5(val sex: String, val sexFull: String) {
+    MALE("М", "Мужской"),
+    FEMALE("Ж", "Женский"),
+    ERROR("unknown", "не опознано"),
 }
