c4cefa900fa2c8441b37b272a38bcd3255092ddf
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-24 16:08:06 +0300

    KS-20-5 24.04.2023 Robot says and modifier
    можно получше можно было сделать?

--- src/main/kotlin/lesson_20/KS-20-5.kt
+++ src/main/kotlin/lesson_20/KS-20-5.kt
@@ -1,2 +1,37 @@
 package lesson_20
 
+
+fun main() {
+
+    val robot = Robot20_5()
+    repeat(3) {
+        robot.say()
+    }
+    println()
+    robot.setModifier { phrase -> phrase.split(" ").asReversed().joinToString(" ") }
+    repeat(3) {
+        robot.say()
+    }
+}
+
+class Robot20_5 {
+    private var modifier: ((String) -> String)? = null
+
+    fun say() {
+        val phrases = listOf(
+            "добрый день",
+            "Выбрасывай мусор в урну",
+            "Мой руки перед едой",
+            "Не топчи газоны",
+            "УБИТЬ ВСЕХ ЛЮДЕЙ!"
+        )
+        val phrase = phrases.random()
+        val modifiedPhrase = if (modifier != null) modifier!!(phrase)
+        else phrase
+        println(modifiedPhrase)
+    }
+
+    fun setModifier(modifier: (String) -> String) {
+        this.modifier = modifier
+    }
+}
