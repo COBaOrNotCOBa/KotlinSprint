a0205d2c40c42aceced397ae3b1b37470fbd1f59
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-24 11:46:42 +0300

    KS-20-2 24.04.2023 Health potion

--- src/main/kotlin/lesson_20/KS-20-2.kt
+++ src/main/kotlin/lesson_20/KS-20-2.kt
@@ -1,2 +1,23 @@
 package lesson_20
 
+fun main() {
+
+    val player = Player20_2(
+        "COBa",
+        5,
+        10,
+    )
+
+    println("Игрок ${player.name}, здоровье ${player.currentHealth}/${player.maximumHealth}")
+    player.currentHealth = player.takeHealthPotion()
+    println("Игрок ${player.name}, здоровье ${player.currentHealth}/${player.maximumHealth}")
+
+}
+
+class Player20_2(
+    val name: String,
+    var currentHealth: Int,
+    val maximumHealth: Int,
+) {
+    val takeHealthPotion: () -> Int = fun(): Int = maximumHealth
+}
