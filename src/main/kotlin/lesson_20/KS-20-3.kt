62ae84264d8cbc149bf4ef1d7dd52af758398bdc
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-25 10:36:45 +0300

    KS-20-3 25.04.2023 изменил на лямбду вместо анонимной. назвал более корректно переменные.

--- src/main/kotlin/lesson_20/KS-20-3.kt
+++ src/main/kotlin/lesson_20/KS-20-3.kt
@@ -4,15 +4,15 @@ fun main() {
 
     val player = Player20_3()
 
-    val checkPlayerKey: (Boolean) -> Unit = fun(haveKey: Boolean): Unit =
-        if (haveKey) println("Игрок открыл дверь")
+    val playerHaveKeyOfDoor: (Boolean) -> Unit = {
+        if (it) println("Игрок открыл дверь")
         else println("У вас нет нужного ключа")
+    }
 
-    checkPlayerKey(player.haveKeyOfDoor)
+    playerHaveKeyOfDoor(player.haveKeyOfDoor)
     player.haveKeyOfDoor = true
-    checkPlayerKey(player.haveKeyOfDoor)
+    playerHaveKeyOfDoor(player.haveKeyOfDoor)
 
 }
 
 class Player20_3(var haveKeyOfDoor: Boolean = false)
-
