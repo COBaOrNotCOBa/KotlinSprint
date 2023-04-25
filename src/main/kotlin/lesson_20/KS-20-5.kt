1703fce65b790abc63b8c014f7b60cc149678af5
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-25 12:18:33 +0300

    KS-20-3 25.04.2023 1. Вынес список фраз в поле класса 2. modifier инициализировал не null 3. немного убрал лишнего строки, переменные, скобки. 4. в main создал переменную с лямбдой и уже в функцию её вызвал
    теперь выглядит получше код )

--- src/main/kotlin/lesson_20/KS-20-5.kt
+++ src/main/kotlin/lesson_20/KS-20-5.kt
@@ -4,32 +4,34 @@ package lesson_20
 fun main() {
 
     val robot = Robot20_5()
+    val modifier: (String) -> String = { phrase ->
+        phrase.split(" ").asReversed().joinToString(" ")
+    }
+
     repeat(3) {
         robot.say()
     }
+
     println()
-    robot.setModifier { phrase -> phrase.split(" ").asReversed().joinToString(" ") }
+    robot.setModifier(modifier)
+
     repeat(3) {
         robot.say()
     }
 }
 
 class Robot20_5 {
-    private var modifier: ((String) -> String)? = null
+    private var modifier: (String) -> String = { phrase -> phrase }
+    val phrases = listOf(
+        "добрый день",
+        "Выбрасывай мусор в урну",
+        "Мой руки перед едой",
+        "Не топчи газоны",
+        "УБИТЬ ВСЕХ ЛЮДЕЙ!"
+    )
 
-    fun say() {
-        val phrases = listOf(
-            "добрый день",
-            "Выбрасывай мусор в урну",
-            "Мой руки перед едой",
-            "Не топчи газоны",
-            "УБИТЬ ВСЕХ ЛЮДЕЙ!"
-        )
-        val phrase = phrases.random()
-        val modifiedPhrase = if (modifier != null) modifier!!(phrase)
-        else phrase
-        println(modifiedPhrase)
-    }
+    fun say() = println(modifier(phrases.random()))
+
 
     fun setModifier(modifier: (String) -> String) {
         this.modifier = modifier
